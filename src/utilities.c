b61d1e5bb62d99057a097d8d4301a1ed47a30f91
AlexWent <alexwent91@gmail.com>
2016-02-04 11:50:57 +0100

    timing function added

--- src/utilities.c
+++ src/utilities.c
@@ -1,8 +1,21 @@
 #include "utilities.h"
+#include <sys/time.h>
+
 
 /* a few physical constants */
 extern const double mvsq2e; 	/* m*v^2 in kcal/mol */
 extern const double kboltz;     /* boltzman constant in kcal/mol/K */
+
+double seconds(){
+
+/* Return the second elapsed since Epoch (00:00:00 UTC, January 1, 1970) */
+  struct timeval tmp;
+  double sec;
+  gettimeofday( &tmp, (struct timezone *)0 );
+  sec = tmp.tv_sec + ((double)tmp.tv_usec)/1000000.0;
+  return sec;
+}
+
 
 /* compute kinetic energy */
 void ekin(mdsys_t *sys)
