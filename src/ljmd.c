0300f56bb32c7e36e368c5c1b6cd9cc8d161b771
AlexWent <alexwent91@gmail.com>
2016-02-04 12:31:29 +0100

    fixed issue with the seconds() function

--- src/ljmd.c
+++ src/ljmd.c
@@ -10,6 +10,7 @@
 #include <ctype.h>
 #include <stdlib.h>
 #include <math.h>
+//#include <sys/time.h>
 
 #include "force.h"
 #include "velverlet.h"
@@ -26,6 +27,7 @@ const double mvsq2e=2390.05736153349; /* m*v^2 in kcal/mol */
 
 int main(int argc, char **argv) 
 {
+    double t1, t2, tot_t;
     int nprint, i;
     char restfile[BLEN], trajfile[BLEN], ergfile[BLEN], line[BLEN];
     FILE *fp,*traj,*erg;
@@ -94,7 +96,7 @@ int main(int argc, char **argv)
     printf("Starting simulation with %d atoms for %d steps.\n",sys.natoms, sys.nsteps);
     printf("     NFI            TEMP            EKIN                 EPOT              ETOT\n");
     output(&sys, erg, traj);
-
+    tot_t=0.0;
     /**************************************************/
     /* main MD loop */
     for(sys.nfi=1; sys.nfi <= sys.nsteps; ++sys.nfi) {
@@ -105,7 +107,10 @@ int main(int argc, char **argv)
 
         /* propagate system and recompute energies */
         velverlet_1(&sys);
+	t1=seconds();
         force(&sys);
+	t2=seconds();
+	tot_t+=t2-t1;
         velverlet_2(&sys);
         ekin(&sys);
     }
@@ -113,6 +118,7 @@ int main(int argc, char **argv)
 
     /* clean up: close files, free memory */
     printf("Simulation Done.\n");
+    printf("Calls to the force function took %lf seconds total.\n", tot_t);
     fclose(erg);
     fclose(traj);
 
